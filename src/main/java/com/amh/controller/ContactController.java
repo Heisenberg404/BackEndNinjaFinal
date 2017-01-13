15130aaf81bbb99c81b86729e5c5db089e7aa403
Heisenberg404 <andres_montoya@outlook.com>
2017-01-13 07:32:04 -0500

    Commit from SI

---
+++ src/main/java/com/amh/controller/ContactController.java
@@ -0,0 +1,39 @@
+package com.amh.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.amh.constant.ViewConstant;
+import com.amh.model.ContactModel;
+
+@Controller
+@RequestMapping("contacts")
+public class ContactController {
+	
+	private static final Log LOG = LogFactory.getLog(ContactController.class);
+	
+	@GetMapping("/cancel")
+	public String cancel(){
+		
+		return ViewConstant.CONTACTS;
+	}
+	
+	@GetMapping("/contactform")
+	public String redirectContactForm(Model model){
+		model.addAttribute("contactModel", new ContactModel());
+		return ViewConstant.CONTACT_FORM;
+	}
+	
+	@PostMapping("/addcontact")
+	public String addContact(@ModelAttribute(name="contactModel") ContactModel contactModel, Model model){
+		LOG.info("METHOD: addContact() -- params: "+ contactModel.toString());
+		model.addAttribute("result", 1);
+		return ViewConstant.CONTACTS;
+	}
+}
